34cbe131e905d60a8f61ddb6b554726aff965e40
Cassandra Largosa <cjlargos@hawaii.edu>
2017-02-25 19:38:11 -1000

    Task 4: Tasklist authentication

--- app/tasklist.js
+++ app/tasklist.js
@@ -2,34 +2,49 @@ Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
   Template.tasks.helpers({
-    tasks: function () {
+    tasks: function() {
       return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
   Template.tasks.events({
-    "submit .add-task": function (event) {
+    "submit .add-task": function(event) {
       let name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
 
       return false;
     },
-    "click .delete-task": function (event) {
-      if (confirm('Delete Task?')){
-        Tasks.remove(this._id);
+
+    "click .delete-task": function(event) {
+      if (confirm('Delete Task?')) {
+        Meteor.call('deleteTask', this._id);
       }
 
       return false;
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if (!Meteor.userId()) {
+      throw new Meteor.Error('No Access');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId) {
+    Tasks.remove(taskId);
+  }
+})
